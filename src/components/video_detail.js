f96a2bca858a2d8e41390f374e93b9d6442d0b78
Sattar Hummatli <settarxan@gmail.com>
2017-12-03 12:44:18 +0400

    change video details and prepare for deploy

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -10,7 +10,7 @@ const VideoDetail = ({video}) => {
 
   return (
     <div className="video-detail col-md-8">
-      <div clasName="embed-responsive embed-responsive-16by9">
+      <div className="embed-responsive embed-responsive-16by9">
         <iframe className="embed-responsive-item" src={url}></iframe>
       </div>
       <div className="details">
